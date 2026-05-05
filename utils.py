079e6ee8a5ca7504ff2e56d9655a892ec57b34cd
Michal <michal.weigle@gmail.com>
2026-05-05 18:37:08 +0200

    Fixed comments

--- utils.py
+++ utils.py
@@ -1,15 +1,15 @@
-'''This function adds a to b'''
 def add(a: int, b: int) -> int:
+    """Add two integers and return the sum."""
     return a + b
 
-'''This function subtracts b from a'''
 def subtract(a: int, b: int) -> int:
+    """Subtract b from a and return the result."""
     return a - b
 
-'''This function multiplies a by b'''
 def multiply(a: int, b: int) -> int:
+    """Multiply a by b and return the product."""
     return a * b
 
-'''This function divides a by b'''
 def divide(a: int, b: int) -> float:
+    """Divide a by b and return the quotient."""
     return a / b
