a9b4467630d739c1b826c63132b2f0d5162af724
Michal <michal.weigle@gmail.com>
2026-05-05 18:34:40 +0200

    Added comments

--- utils.py
+++ utils.py
@@ -1,15 +1,15 @@
-# Exemplary calculator functions
+'''This function adds a to b'''
 def add(a: int, b: int) -> int:
     return a + b
 
-
+'''This function subtracts b from a'''
 def subtract(a: int, b: int) -> int:
     return a - b
 
-
+'''This function multiplies a by b'''
 def multiply(a: int, b: int) -> int:
     return a * b
 
-
+'''This function divides a by b'''
 def divide(a: int, b: int) -> float:
     return a / b
